7e2fa7f394fb1ddbfe845c6c95221212cd5ec580
Mafalda <mf.batalha@campus.fct.unl.pt>
2022-11-14 18:57:49 +0000

    a

---
+++ project-2/app.js
@@ -0,0 +1,119 @@
+import { buildProgramFromSources, loadShadersFromURLS, setupWebGL } from "../../libs/utils.js";
+import { ortho, lookAt, flatten } from "../../libs/MV.js";
+import {modelView, loadMatrix, multRotationX, multRotationY, multRotationZ, multScale, pushMatrix, popMatrix, multTranslation } from "../../libs/stack.js";
+
+import * as SPHERE from '../../libs/objects/sphere.js';
+import * as CUBE from '../../libs/objects/cube.js';
+import * as PYRAMID from '../../libs/objects/pyramid.js';
+import * as CYLINDER from '../../libs/objects/cylinder.js';
+import * as BUNNY from '../../libs/objects/bunny.js';
+
+/** @type WebGLRenderingContext */
+let gl;
+
+let time = 0;           // Global simulation time in days
+let speed = 0.005;     // Speed (how many days added to time on each render pass
+let mode;               // Drawing mode (gl.LINES or gl.TRIANGLES)
+let animation = true;   // Animation is running
+
+
+function setup(shaders)
+{
+    let canvas = document.getElementById("gl-canvas");
+    let aspect = canvas.width / canvas.height;
+
+    gl = setupWebGL(canvas);
+
+    let program = buildProgramFromSources(gl, shaders["shader.vert"], shaders["shader.frag"]);
+
+    let mProjection = ortho(-aspect,aspect, -1, 1,-3,3);
+
+    mode = gl.TRIANGLES; 
+
+    resize_canvas();
+    window.addEventListener("resize", resize_canvas);
+
+    document.onkeydown = function(event) {
+        switch(event.key) {
+            case 'w':
+                mode = gl.LINES; 
+                break;
+            case 's':
+                mode = gl.TRIANGLES;
+                break;
+            case 'p':
+                animation = !animation;
+                break;
+            case '+':
+                if(animation) speed *= 1.1;
+                break;
+            case '-':
+                if(animation) speed /= 1.1;
+                break;
+        }
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    PYRAMID.init(gl);
+    SPHERE.init(gl);
+    CUBE.init(gl);
+    CYLINDER.init(gl);
+    gl.enable(gl.DEPTH_TEST);   // Enables Z-buffer depth test
+    
+    window.requestAnimationFrame(render);
+
+
+    function resize_canvas(event)
+    {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        aspect = canvas.width / canvas.height;
+
+        gl.viewport(0,0,canvas.width, canvas.height);
+        mProjection = ortho(-aspect,aspect, -1, 1,-3,3);
+    }
+
+    function uploadModelView()
+    {
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mModelView"), false, flatten(modelView()));
+    }
+
+    function blade()
+    {
+        multRotationZ(-90);
+        multScale([0.1, 0.5, 0.01]);
+        uploadModelView();
+        PYRAMID.draw(gl, program, mode);
+    }
+
+    function render()
+    {
+        if(animation) time += speed;
+        window.requestAnimationFrame(render);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        
+        gl.useProgram(program);
+        
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mProjection"), false, flatten(mProjection));
+    
+        loadMatrix(lookAt([0,0,1], [0,0,0], [0,1,0]));
+
+        helicopter();
+    }
+
+    function helicopter() {
+        pushMatrix();
+            multTranslation([-0.25, 0.0, 0.0])
+            multRotationY(360*time);
+            blade();
+            blade();
+        popMatrix();
+
+    }
+
+}
+
+const urls = ["shader.vert", "shader.frag"];
+loadShadersFromURLS(urls).then(shaders => setup(shaders))
